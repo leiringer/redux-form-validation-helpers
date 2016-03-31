1daa60335beef7d1316fe12dedac994e3a7bce01
Peter Leiringer <leiringer@gmail.com>
2016-03-31 09:14:51 +0200

    fixed entry point
    updated package.json with entry point and repository. added index entry
    point

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import  {
+	addValidation,
+	addMultipleValidations,
+	getValidators,
+	getFieldStrings,
+	getFieldObjects,
+	generateAsyncBlurFields,
+	generateReduxFormConfiguration,
+	generateAsyncValidation
+} from './helpers.js';
+
+export {
+	addValidation,
+	addMultipleValidations,
+	getValidators,
+	getFieldStrings,
+	getFieldObjects,
+	generateAsyncBlurFields,
+	generateReduxFormConfiguration,
+	generateAsyncValidation
+};
